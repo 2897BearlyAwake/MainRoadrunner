3c2399287c4314cd563918cf1f7ae8c3ae3692b3
robotics <jamesbeam@gfs.org>
2024-11-06 17:26:34 -0500

    3 degrees of motion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
@@ -18,21 +18,30 @@ public class TestClass extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         // Position of the arm when it's lifted
-        int armUpPosition = 1000;
+        int armUpPosition = 1270;
+        int slideoutPosition = 600;
 
         // Position of the arm when it's down
         int armDownPosition = 0;
+        int slideinPosition = 20;
+
+        int wristUpPosition = 0;
+        int wristDownPosition = 200;
 
         // Find a motor in the hardware map named "Arm Motor"
         DcMotor armMotor = hardwareMap.dcMotor.get("arm");
+        DcMotor slideMotor = hardwareMap.dcMotor.get("slide");
+        DcMotor wristMotor = hardwareMap.dcMotor.get("wrist");
+
 
         // Reset the motor encoder so that it reads zero ticks
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wristMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         // Sets the starting position of the arm to the down position
-        armMotor.setTargetPosition(armDownPosition);
-        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        armMotor.setTargetPosition(armDownPosition);
+//        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         waitForStart();
 
@@ -42,6 +51,34 @@ public class TestClass extends LinearOpMode {
         armMotor.setTargetPosition(armUpPosition);
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armMotor.setPower(0.5);
+
+        sleep(2050);
+
+        slideMotor.setTargetPosition(slideoutPosition);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slideMotor.setPower(1);
+
+        sleep(5000);
+
+        wristMotor.setTargetPosition(wristDownPosition);
+        wristMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wristMotor.setPower(.5);
+
+        sleep(2000);
+
+        wristMotor.setTargetPosition(wristUpPosition);
+        wristMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wristMotor.setPower(.5);
+
+        sleep(2000);
+
+        slideMotor.setTargetPosition(slideinPosition);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slideMotor.setPower(1);
+
+        sleep(5000);
+
+        slideMotor.setPower(0);
 
         // Get the current position of the armMotor
         double position = armMotor.getCurrentPosition();
@@ -57,7 +94,7 @@ public class TestClass extends LinearOpMode {
 
         telemetry.update();
 
-        sleep(10000);
+        sleep(1000);
         //sleepy
 
         armMotor.setTargetPosition(armDownPosition);
